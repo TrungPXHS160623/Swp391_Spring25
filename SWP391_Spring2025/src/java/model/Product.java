98bc266f0862d6e899b5349c9a117bd83ecbed8c
NDDoan <108049904+NDDoan@users.noreply.github.com>
2025-02-14 19:59:26 +0700

    Model.Product

---
+++ SWP391_Spring2025/src/java/model/Product.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Product {
+    private int product_id ;
+    private String product_name;
+    private int subcategory_id;
+    private Date created_at;
+    private Date updated_at;
+    private int status;
+    private float list_price;
+    private float sale_price;
+    private int featured;
+
+    public Product() {
+    }
+
+    public Product(int product_id, String product_name, int subcategory_id, Date created_at, Date updated_at, int status, float list_price, float sale_price, int featured) {
+        this.product_id = product_id;
+        this.product_name = product_name;
+        this.subcategory_id = subcategory_id;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.status = status;
+        this.list_price = list_price;
+        this.sale_price = sale_price;
+        this.featured = featured;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public int getSubcategory_id() {
+        return subcategory_id;
+    }
+
+    public void setSubcategory_id(int subcategory_id) {
+        this.subcategory_id = subcategory_id;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public float getList_price() {
+        return list_price;
+    }
+
+    public void setList_price(float list_price) {
+        this.list_price = list_price;
+    }
+
+    public float getSale_price() {
+        return sale_price;
+    }
+
+    public void setSale_price(float sale_price) {
+        this.sale_price = sale_price;
+    }
+
+    public int getFeatured() {
+        return featured;
+    }
+
+    public void setFeatured(int featured) {
+        this.featured = featured;
+    }
+    
+    
+}
