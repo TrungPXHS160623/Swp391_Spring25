40b5a8520b1bf965944f9f3650671b10cf1e59e6
NDDoan <108049904+NDDoan@users.noreply.github.com>
2025-02-19 22:36:44 +0700

    Update ProductDAO.java

--- SWP391_Spring2025/src/java/Dao/ProductDAO.java
+++ SWP391_Spring2025/src/java/Dao/ProductDAO.java
@@ -38,6 +38,25 @@ public class ProductDAO {
         return products;
     }
 
+    public List<Product> getAllProduct(int page, int limit) {
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * FROM Products ORDER BY product_id DESC OFFSET ? ROWS FETCH NEXT ? ROWS ONLY";
+        try (Connection conn = new DBContext().getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, (page - 1) * limit);
+            stmt.setInt(2, limit);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    products.add(mapResultSetToProduct(rs));
+                }
+            }
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Lỗi khi phân trang danh mục", e);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Lỗi kết nối CSDL khi Phân trang danh mục", e);
+        }
+        return products;
+    }
+
     // Chuyển đổi từ ResultSet thành Product Object
     private Product mapResultSetToProduct(ResultSet rs) throws SQLException {
         Product product = new Product();
@@ -54,7 +73,7 @@ public class ProductDAO {
 
     public static void main(String[] args) {
         ProductDAO pDAO = new ProductDAO();
-        
+
         //Tính tổng sổ product hiện có
         List<Product> products = pDAO.getAllProduct();
         System.out.println("Tổng số danh mục: " + products.size());
@@ -71,5 +90,10 @@ public class ProductDAO {
             System.out.println("Giá bán: " + product.getSale_price());
             System.out.println("Sản phẩm nổi bật: " + product.getFeatured());
         }
+
+        //Test phân trang danh mục (Kiểm tra tối đa 5)
+        List<Product> paginatedCategories = pDAO.getAllProduct(1, 5);
+        System.out.println("////////////////////////////////////////////////////////");
+        System.out.println("Danh mục trang 1 (tối đa 5): " + paginatedCategories.size());
     }
 }
